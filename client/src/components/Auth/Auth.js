85d407ad21fec7b78e7acefcfb97304540572ea1
Carlos Abril <carlos.abril87@outlook.com>
2021-09-09 18:44:15 -0400

    first deployed test

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -29,7 +29,8 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'http://localhost:5000/auth';
+        // const URL = 'http://localhost:5000/auth';
+        const URL = 'https://react-chatnow.herokuapp.com/auth'
 
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
